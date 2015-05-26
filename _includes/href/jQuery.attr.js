bbeafab98c8c7d7bace6e5ca55ed1e73e7f93c9c
thxopen <keith@thxopen.com>
2015-05-26 14:01:03 +0800

    增加服务器处理手册

---
+++ _includes/href/jQuery.attr.js
@@ -0,0 +1,3 @@
+<a href="http://api.jquery.com/attr/">
+    <code class="type"  title="Javascript parameter type">jQuery.attr()</code>
+</a>
---
+++ _includes/href/jQuery.data.js
@@ -0,0 +1,3 @@
+<a href="http://api.jquery.com/data/">
+    <code class="type"  title="Javascript parameter type">jQuery.data()</code>
+</a>
